e64f338623575db088530b0b1eb73058cfcf8852
svetlio44 <99183797+svetlio44@users.noreply.github.com>
2024-06-05 14:01:53 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace MultiThreadedTimersApp
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(374, 224);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Стартирай таймерите";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(922, 487);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "MultiThreadedTimersApp";
+            this.ResumeLayout(false);
+
+        }
+
+        private System.Windows.Forms.Timer timer3;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultiThreadedTimersApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // Код за изпълнение при всяко "тикване" на таймера 1
+            MessageBox.Show("Таймер 1 активен!", "Информация");
+        }
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            // Код за изпълнение при всяко "тикване" на таймера 2
+            MessageBox.Show("Таймер 2 активен!", "Информация");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Стартиране на таймерите в различни нишки
+            Task.Run(() => timer1.Start());
+            Task.Run(() => timer2.Start());
+        }
+
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
